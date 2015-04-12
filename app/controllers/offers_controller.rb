dbaca07dbdd7747ee458fe27834e5c256d21f5f0
Daniel Dancziger <ddancziger@gmail.com>
2015-04-12 15:20:36 +0300

    offer edit params

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,15 +1,27 @@
 class OffersController < InheritedResources::Base
+	before_action :set_offer, only: [:edit, :update, :destroy]
+	def new
+		@offer = Offer.new
+		@offer.build_computer_spec
+		@offer.build_camera_spec
+	end
 
 	def create
-		@offer = Offer.new(offer_params)
+
+		#Saving Parameters Depending on Which SubCategory -> Specs Of the Offer!
+		@offer = Offer.new(offer_params(params[:subcategory_id]))
+
+		#Saving the current user to the offer
 		if current_user
 			@offer.user_id = current_user.id
 		end
 
+		#Connecting the Offer to the Product
 		if params['product_id']
 			@offer.product_id = params['product_id']
 		end
 
+		#render :text => "<pre>" + offer_params[:offer].to_s and return
 		respond_to do |format|
 			if @offer.save
 
@@ -22,10 +34,37 @@ class OffersController < InheritedResources::Base
 		end
 	end
 
-  private
 
-    def offer_params
-      params.require(:offer).permit(:title,:price,:expire,:product_id,:description,:condition,:currency,:brand_id)
-    end
+	def update
+		#render :text => "<pre>" + offer_params(@offer.product.subcategory_id.to_s).to_s and return
+		respond_to do |format|
+
+			if @offer.update(offer_params(@offer.product.subcategory_id.to_s))
+				format.html { redirect_to  @offer, notice: 'Offer was successfully updated.' }
+				format.json { render :show, status: :ok, location: @offer }
+			else
+				format.html { render :edit }
+				format.json { render json: @offer.errors, status: :unprocessable_entity }
+			end
+		end
+	end
+
+
+  private
+		# Use callbacks to share common setup or constraints between actions.
+		def set_offer
+			@offer = Offer.find(params[:id])
+		end
+		#Permit Params to save or manipulate depending which type of specs we need
+		def offer_params sub_category
+			if sub_category.eql?('1')
+				params.require(:offer).permit(:title,:price,:expire,:product_id,:description,:condition,:currency,:brand_id,
+				                              computer_spec_attributes: [:hard,:ram,:processor,:monitor])
+			else
+				params.require(:offer).permit(:title,:price,:expire,:product_id,:description,:condition,:currency,:brand_id,
+				                              camera_spec_attributes: [:mega_pixel,:type,:optical_zoom])
+			end
+		end
+
 end
 
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -5,4 +5,6 @@ class Offer < ActiveRecord::Base
 	belongs_to :brand
 	belongs_to :computer_spec
 	belongs_to :camera_spec
+	accepts_nested_attributes_for :computer_spec
+	accepts_nested_attributes_for :camera_spec
 end
